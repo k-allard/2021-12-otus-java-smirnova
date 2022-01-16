5a6a6a362eccbe5660fa81c86b1316ffe0ff0420
kallard <kallard@student.21-school.ru>
2022-01-16 16:14:35 +0300

    Change target name of shadowJar archive

--- HW01-gradle/src/main/java/com/company/HelloOtus.java
+++ HW01-gradle/src/main/java/com/company/HelloOtus.java
@@ -7,11 +7,11 @@ import com.google.common.collect.Multimap;
  *
  * To start the application:
  * ./gradlew build
- * java -jar ./HW01-gradle/build/libs/gradleHelloWorld-0.1.jar
+ * java -jar ./HW01-gradle/build/libs/gradleShadowJarArchive-0.1.jar
  *
  * To unzip the jar:
  * unzip -l HW01-gradle.jar
- * unzip -l gradleHelloWorld-0.1.jar
+ * unzip -l gradleShadowJarArchive-0.1.jar
  *
  */
 public class HelloOtus {
