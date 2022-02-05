1fae9d2edc9e11a7ae2f245e07bfee310250193a
kallard <kallard@student.21-school.ru>
2022-02-06 02:25:13 +0300

    Draft

--- HW06-annotations/src/main/java/com/company/MyJUnit.java
+++ HW06-annotations/src/main/java/com/company/MyJUnit.java
@@ -1,7 +1,15 @@
 package com.company;
 
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.Multimap;
+import com.company.annotations.After;
+import com.company.annotations.Before;
+import com.company.annotations.Test;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -11,7 +19,99 @@ import com.google.common.collect.Multimap;
  *
  */
 public class MyJUnit {
-    public static void main(String[] args) {
 
+    public static void launch(Class<?> testClass) {
+//        boolean beforeMethodPerformed = false;
+        int failedTests = 0;
+        int successTests = 0;
+
+        Method[] methods = testClass.getMethods();
+        Map<Boolean, List<Method>> testMethodsMap =
+                Arrays.stream(methods).collect(
+                        Collectors.partitioningBy(
+                                method -> method.getClass().isAnnotationPresent(Test.class)
+//                                        && Modifier.isPublic(method.getModifiers())
+                        )
+                );
+
+        List<Method> notTestMethods = testMethodsMap.get(false);
+        List<Method> beforeMethods = notTestMethods.stream()
+                .filter(method -> method.getClass().isAnnotationPresent(Before.class)
+                        && Modifier.isPublic(method.getModifiers())
+                )
+                .collect(Collectors.toList());
+
+        List<Method> afterMethods = notTestMethods.stream()
+                .filter(method -> method.getClass().isAnnotationPresent(After.class)
+                        && Modifier.isPublic(method.getModifiers())
+                )
+                .collect(Collectors.toList());
+
+        for (Method beforeMethod : beforeMethods) {
+            performBeforeOrAfterMethod(beforeMethod, testClass);
+        }
+
+        for (Method testMethod : testMethodsMap.get(true)) {
+            try {
+                performTestMethod(testMethod, testClass);
+                successTests++;
+            } catch (InvocationTargetException e) {
+                System.err.println(e.getCause().getMessage());
+                failedTests++;
+            }
+        }
+
+        for (Method afterMethod : afterMethods) {
+            performBeforeOrAfterMethod(afterMethod, testClass);
+        }
+
+
+
+
+//        Map<Boolean, List<Method>> testMethodsMap =
+//                Arrays.stream(methods).collect(Collectors.partitioningBy(method -> method.getClass().isAnnotationPresent(Test.class)));
+//
+//        Map<Boolean, List<Method>> afterMethodsMap =
+//                Arrays.stream(methods).collect(Collectors.partitioningBy(method -> method.getClass().isAnnotationPresent(After.class)));
+//
+//        for (Method method : methods) {
+//            if (!beforeMethodPerformed && method.isAnnotationPresent(Before.class)) {
+//                performBeforeOrAfterMethod(method, testClass);
+//                beforeMethodPerformed = true;
+//                continue;
+//            }
+//            if (method.isAnnotationPresent(Test.class)) {
+//                try {
+//                    performTestMethod(method, testClass);
+//                    successTests++;
+//                } catch (InvocationTargetException e) {
+//                    System.err.println(e.getCause().getMessage());
+//                    failedTests++;
+//                }
+//                continue;
+//            }
+//            if (method.isAnnotationPresent(After.class)) {
+//                performBeforeOrAfterMethod(method, testClass);
+//            }
+//        }
+        System.out.println("_________________________");
+        System.out.println("Success tests number: [" + successTests + "]");
+        System.out.println("Failed tests number: [" + failedTests + "]");
+    }
+
+    private static void performTestMethod(Method method, Class<?> testClass) throws InvocationTargetException {
+        try {
+            method.invoke(testClass.getDeclaredConstructor().newInstance());
+        } catch (IllegalAccessException | NoSuchMethodException | InstantiationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void performBeforeOrAfterMethod(Method method, Class<?> testClass)  {
+        try {
+            method.invoke(testClass.getDeclaredConstructor().newInstance());
+        } catch (IllegalAccessException | NoSuchMethodException | InstantiationException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ HW06-annotations/src/main/java/com/company/TestClass.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import com.company.annotations.After;
+import com.company.annotations.Before;
+import com.company.annotations.Test;
+
+public class TestClass {
+    @Before
+    public void init() {
+        System.out.println("Before init() method called");
+    }
+
+    @Test
+    public void successTest1() {
+        System.out.println("Done successTest1()");
+    }
+
+    @Test
+    public void failureTest() {
+        throw new RuntimeException("Exception from failureTest()");
+    }
+
+    @Test
+    public void successTest2() {
+        System.out.println("Done successTest2()");
+    }
+
+    @After
+    public void cleanUp() {
+        System.out.println("After cleanUp() method called");
+    }
+}
